c0e4f812281600fe85350b8c0e053c64a295ef41
hovaheb <hojat.vaheb@gmail.com>
2017-01-23 15:45:36 +0800

    from Work

--- src/main/java/org/abshar/learning/patterns/strategy/MiniDuckSimulator.java
+++ src/main/java/org/abshar/learning/patterns/strategy/MiniDuckSimulator.java
@@ -15,7 +15,7 @@ public class MiniDuckSimulator {
 		Duck.DecoyDuck decoy = new Duck.DecoyDuck();
  
 		ModelDuck model = new ModelDuck();
-		
+
 		mallard.performQuack();
 		rubberDuckie.performQuack();
 		decoy.performQuack();
